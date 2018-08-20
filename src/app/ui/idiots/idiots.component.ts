66c744dd9d4b73ca41a5b7b0988fe9d7bb11b180
Josh Odom <jodom@Joshs-MBP.centurylink.net>
2018-08-19 21:17:39 -0500

    manager card comp looking goood

--- src/app/ui/idiots/idiots.component.ts
+++ src/app/ui/idiots/idiots.component.ts
@@ -64,14 +64,10 @@ export class IdiotsComponent implements OnInit {
         this.cameronDrafted(),
         this.cameronFinal(),
         'No One Fights Alone',
-        1
+        1,
+        "Cameron isn't terrible like the rest of you assholes"
       )
     );
-    // let myTeam = new Team("qb", "rb", "rb2", "wr1", "wr2", "te", "flex", "k", "def", "bn1", "bn2", "bn3", "bn4", "bn5", "bn6");
-    // let finalTeam = new Team("qbz", "rbz", "rb2z", "wr1z", "wr2z", "tez", "flexz", "kz", "defz", "bn1z", "bn2z", "bn3z", "bn4z", "bn5z", "bn6z");
-    // this.managers.push(new Manager(myTeam, finalTeam, "Test Team", 1));
-    // this.managers.push(new Manager(myTeam, finalTeam, "Test Team2", 2));
-    // this.managers.push(new Manager(myTeam, finalTeam, "Test Team3", 7));
   }
 
   cameronDrafted(): Team {
--- src/app/ui/models/manager.ts
+++ src/app/ui/models/manager.ts
@@ -5,10 +5,12 @@ export class Manager {
     finalTeam: Team;
     teamName: string;
     place: number;
-    constructor(_draftedTeam: Team, _finalTeam: Team, _name: string, _place: number) {
+    bio: string;
+    constructor(_draftedTeam: Team, _finalTeam: Team, _name: string, _place: number, _bio: string) {
         this.draftedTeam = _draftedTeam;
         this.finalTeam = _finalTeam;
         this.teamName = _name;
         this.place = _place;
+        this.bio = _bio;
     }
 }
