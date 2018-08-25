9bf0e1dd9746adbc6347d3a1cca3333acc1f457e
unknown <jodom0303@gmail.com>
2018-08-25 13:16:49 -0500

    finished setting up teams!

--- src/app/ui/idiots/idiots.component.ts
+++ src/app/ui/idiots/idiots.component.ts
@@ -82,14 +82,122 @@ export class IdiotsComponent implements OnInit {
     );
     this.managers.push(
       new Manager(
+        this.berhorstDrafted(),
+        this.berhorstFinal(),
+        'MIZZOU',
+        3,
+        "I cannot stress enough how much I hate that this team is third.",
+        "8-6-0",
+        "1307.30",
+        "1256.18"
+      )
+    );
+    this.managers.push(
+      new Manager(
         this.orschelnDrafted(),
         this.orschelnFinal(),
         'Just Saiyan...',
-        3,
+        4,
         "What a mediocre team, yet here we are..",
         "8-6-0",
         "1362.32",
         "1369.78"
+      )
+    );
+    this.managers.push(
+      new Manager(
+        this.benDrafted(),
+        this.benFinal(),
+        'Smallville High',
+        5,
+        "Quite possibly the worst team to EVER end the season .500",
+        "7-7-0",
+        "1221.90",
+        "1148.06"
+      )
+    );
+    this.managers.push(
+      new Manager(
+        this.mikeDrafted(),
+        this.mikeFinal(),
+        'Dank Memes',
+        6,
+        "Seriously, fuck Bilal Powell and everything about him.",
+        "8-6-0",
+        "1224.38",
+        "1283.82"
+      )
+    );
+    this.managers.push(
+      new Manager(
+        this.joshDrafted(),
+        this.joshFinal(),
+        'The Airplanes',
+        7,
+        "I'm pretty sure if he could pick every Seahawks player for his team, he would.",
+        "8-6-0",
+        "1251.30",
+        "1249.08"
+      )
+    );
+    this.managers.push(
+      new Manager(
+        this.ajDrafted(),
+        this.ajFinal(),
+        'Just End The Season',
+        8,
+        "'That trade doesn't benefit me enough' might as well be his team name.",
+        "7-7-0",
+        "1276.04",
+        "1237.66"
+      )
+    );
+    this.managers.push(
+      new Manager(
+        this.ryanDrafted(),
+        this.ryanFinal(),
+        'PutSomeMustardOnIt',
+        9,
+        "Probably the least enthusiastic manager in the league.",
+        "6-8-0",
+        "1367.28",
+        "1280.24"
+      )
+    );
+    this.managers.push(
+      new Manager(
+        this.robbDrafted(),
+        this.robbFinal(),
+        'Lost Pollos Hermanos',
+        10,
+        "Robb 'My brother cheated for me and I still got 10th place' McCleary. What more can you say?",
+        "6-8-0",
+        "1270.72",
+        "1329.10"
+      )
+    );
+    this.managers.push(
+      new Manager(
+        this.steveDrafted(),
+        this.steveFinal(),
+        'The PoopDicks',
+        11,
+        "Technically last place by every standard except this stupid fucking league, Steve had a tough time this year.",
+        "4-10-0",
+        "1277.94",
+        "1407.20"
+      )
+    );
+    this.managers.push(
+      new Manager(
+        this.meDrafted(),
+        this.meFinal(),
+        'Who fucking knows',
+        12,
+        "If all of the other teams combined and had a buffet, then shit out a team, it wouldn't be as bad as my team was last year.",
+        "4-10-0",
+        "1151.42",
+        "1306.72"
       )
     );
   }
@@ -251,17 +359,6 @@ export class IdiotsComponent implements OnInit {
     myTeam.addPlayer(new Player('Delanie Walker', Teams.Tennessee, Positions.TE));
     myTeam.addPlayer(new Player('Jake Elliott', Teams.Philadelphia, Positions.K));
     myTeam.addPlayer(new Player('Arizona', Teams.Arizona, Positions.DEF));
-    
-    
-    // myTeam.addPlayer(new Player('Terrelle Pryor Sr.', Teams.NYJ, Positions.WR));
-    // myTeam.addPlayer(new Player('Ameer Abdullah', Teams.Detroit, Positions.RB));
-    // myTeam.addPlayer(new Player('Davante Adams', Teams.GreenBay, Positions.WR));
-    // myTeam.addPlayer(new Player('Darren McFadden', Teams.Dallas, Positions.RB));
-    // myTeam.addPlayer(new Player('John Brown', Teams.Baltimore, Positions.WR));
-    // myTeam.addPlayer(new Player('James White', Teams.NewEngland, Positions.RB));
-    // myTeam.addPlayer(new Player('Zay Jones', Teams.Buffalo, Positions.WR));
-    // myTeam.addPlayer(new Player('Brandon McManus', Teams.Denver, Positions.K));
-    // myTeam.addPlayer(new Player('Kenny Britt', Teams.NewEngland, Positions.WR));
     return myTeam;
   }
 
@@ -285,6 +382,26 @@ export class IdiotsComponent implements OnInit {
     return myTeam;
   }
 
+  benFinal(): Team {
+    var myTeam = new Team();
+    myTeam.addPlayer(new Player('Giovani Bernard', Teams.Cincinnati, Positions.RB));
+    myTeam.addPlayer(new Player('Martavis Bryant', Teams.Oakland, Positions.WR));
+    myTeam.addPlayer(new Player('Jared Cook', Teams.LAR, Positions.TE));
+    myTeam.addPlayer(new Player('Kenyan Drake', Teams.Miami, Positions.RB));
+    myTeam.addPlayer(new Player('Larry Fitzgerald', Teams.Arizona, Positions.WR));
+    myTeam.addPlayer(new Player('A.J. Green', Teams.Cincinnati, Positions.WR));
+    myTeam.addPlayer(new Player('Rob Gronkowski', Teams.NewEngland, Positions.TE));
+    myTeam.addPlayer(new Player('Alfred Morris', Teams.Dallas, Positions.RB));
+    myTeam.addPlayer(new Player('DeMarco Murray', Teams.Tennessee, Positions.RB));
+    myTeam.addPlayer(new Player('Dak Prescott', Teams.Dallas, Positions.QB));
+    myTeam.addPlayer(new Player('Aaron Rodgers', Teams.GreenBay, Positions.QB));
+    myTeam.addPlayer(new Player('Emmanuel Sanders', Teams.Denver, Positions.WR));
+    myTeam.addPlayer(new Player('Kai Forbath', Teams.Minnesota, Positions.K));
+    myTeam.addPlayer(new Player('Greg Zuerlein', Teams.LAR, Positions.K));
+    myTeam.addPlayer(new Player('Kansas City', Teams.KansasCity, Positions.DEF));
+    return myTeam;
+  }
+
   mikeDrafted(): Team {
     var myTeam = new Team();
     myTeam.addPlayer(new Player('Jordan Howard', Teams.Chicago, Positions.RB));
@@ -302,6 +419,26 @@ export class IdiotsComponent implements OnInit {
     myTeam.addPlayer(new Player('Dion Lewis', Teams.Tennessee, Positions.RB));
     myTeam.addPlayer(new Player('Matt Bryant', Teams.Atlanta, Positions.K));
     myTeam.addPlayer(new Player('Cameron Brate', Teams.TampaBay, Positions.TE));
+    return myTeam;
+  }
+
+  mikeFinal(): Team {
+    var myTeam = new Team();
+    myTeam.addPlayer(new Player('Drew Brees', Teams.NewOrleans, Positions.QB));
+    myTeam.addPlayer(new Player('Tarik Cohen', Teams.Chicago, Positions.RB));
+    myTeam.addPlayer(new Player('Marquise Goodwin', Teams.SanFrancisco, Positions.WR));
+    myTeam.addPlayer(new Player('Jimmy Graham', Teams.GreenBay, Positions.TE));
+    myTeam.addPlayer(new Player('Chris Hogan', Teams.NewEngland, Positions.WR));
+    myTeam.addPlayer(new Player('Jordan Howard', Teams.Chicago, Positions.RB));
+    myTeam.addPlayer(new Player('Jordy Nelson', Teams.Oakland, Positions.WR));
+    myTeam.addPlayer(new Player('Bilal Powell', Teams.NYJ, Positions.RB));
+    myTeam.addPlayer(new Player('Paul Richardson Jr.', Teams.Seattle, Positions.WR));
+    myTeam.addPlayer(new Player('Demaryius Thomas', Teams.Denver, Positions.WR));
+    myTeam.addPlayer(new Player('Jamaal Williams', Teams.GreenBay, Positions.RB));
+    myTeam.addPlayer(new Player('Terrance Williams', Teams.Dallas, Positions.WR));
+    myTeam.addPlayer(new Player('Harrison Butker', Teams.KansasCity, Positions.K));
+    myTeam.addPlayer(new Player('Denver', Teams.Denver, Positions.DEF));
+    myTeam.addPlayer(new Player('Philadelphia', Teams.Philadelphia, Positions.DEF));
     return myTeam;
   }
 
@@ -325,6 +462,26 @@ export class IdiotsComponent implements OnInit {
     return myTeam;
   }
 
+  joshFinal(): Team {
+    var myTeam = new Team();
+    myTeam.addPlayer(new Player('Robby Anderson', Teams.NYJ, Positions.WR));
+    myTeam.addPlayer(new Player('Kelvin Benjamin', Teams.Buffalo, Positions.WR));
+    myTeam.addPlayer(new Player('Alex Collins', Teams.Baltimore, Positions.RB));
+    myTeam.addPlayer(new Player('Eric Ebron', Teams.Indianapolis, Positions.TE));
+    myTeam.addPlayer(new Player('Mike Evans', Teams.TampaBay, Positions.WR));
+    myTeam.addPlayer(new Player('Joe Flacco', Teams.Baltimore, Positions.QB));
+    myTeam.addPlayer(new Player('Carlos Hyde', Teams.Cleveland, Positions.RB));
+    myTeam.addPlayer(new Player('Mark Ingram', Teams.NewOrleans, Positions.RB));
+    myTeam.addPlayer(new Player('Dion Lewis', Teams.Tennessee, Positions.RB));
+    myTeam.addPlayer(new Player('Marcus Mariota', Teams.Tennessee, Positions.QB));
+    myTeam.addPlayer(new Player('Ricky Seals-Jones', Teams.Arizona, Positions.TE));
+    myTeam.addPlayer(new Player('Michael Thomas', Teams.NewOrleans, Positions.WR));
+    myTeam.addPlayer(new Player('Wil Lutz', Teams.NewOrleans, Positions.K));
+    myTeam.addPlayer(new Player('Detroit', Teams.Detroit, Positions.DEF));
+    myTeam.addPlayer(new Player('New England', Teams.NewEngland, Positions.DEF));
+    return myTeam;
+  }
+
   ajDrafted(): Team {
     var myTeam = new Team();
     myTeam.addPlayer(new Player('David Johnson', Teams.Arizona, Positions.RB));
@@ -342,6 +499,26 @@ export class IdiotsComponent implements OnInit {
     myTeam.addPlayer(new Player('Minnesota', Teams.Minnesota, Positions.DEF));
     myTeam.addPlayer(new Player('Deshaun Watson', Teams.Houston, Positions.QB));
     myTeam.addPlayer(new Player('Dan Bailey', Teams.Dallas, Positions.K));
+    return myTeam;
+  }
+
+  ajFinal(): Team {
+    var myTeam = new Team();
+    myTeam.addPlayer(new Player('Keenan Allen', Teams.LAC, Positions.WR));
+    myTeam.addPlayer(new Player('Isaiah Crowell', Teams.NYJ, Positions.RB));
+    myTeam.addPlayer(new Player('Matt Forte', Teams.NYJ, Positions.RB));
+    myTeam.addPlayer(new Player('Jared Goff', Teams.LAR, Positions.QB));
+    myTeam.addPlayer(new Player('Josh Gordon', Teams.Cleveland, Positions.WR));
+    myTeam.addPlayer(new Player('David Johnson', Teams.Arizona, Positions.RB));
+    myTeam.addPlayer(new Player('Aaron Jones', Teams.GreenBay, Positions.RB));
+    myTeam.addPlayer(new Player('Marvin Jones Jr.', Teams.Detroit, Positions.WR));
+    myTeam.addPlayer(new Player('Joe Mixon', Teams.Cincinnati, Positions.RB));
+    myTeam.addPlayer(new Player('Greg Olsen', Teams.Carolina, Positions.TE));
+    myTeam.addPlayer(new Player('Austin Seferian-Jenkins', Teams.NYJ, Positions.TE));
+    myTeam.addPlayer(new Player('Adam Thielen', Teams.Minnesota, Positions.WR));
+    myTeam.addPlayer(new Player('Kerwynn Williams', Teams.Arizona, Positions.RB));
+    myTeam.addPlayer(new Player('Matt Prater', Teams.Detroit, Positions.K));
+    myTeam.addPlayer(new Player('Jacksonville', Teams.Jacksonville, Positions.DEF));
     return myTeam;
   }
 
@@ -365,6 +542,26 @@ export class IdiotsComponent implements OnInit {
     return myTeam;
   }
 
+  ryanFinal(): Team {
+    var myTeam = new Team();
+    myTeam.addPlayer(new Player('Ameer Abdullah', Teams.Detroit, Positions.RB));
+    myTeam.addPlayer(new Player('Nelson Agholor', Teams.Philadelphia, Positions.WR));
+    myTeam.addPlayer(new Player('Antonio Brown', Teams.Pittsburgh, Positions.WR));
+    myTeam.addPlayer(new Player('Orleans Darkwa', Teams.NYG, Positions.RB));
+    myTeam.addPlayer(new Player('Derrick Henry', Teams.Tennessee, Positions.RB));
+    myTeam.addPlayer(new Player('Tyreek Hill', Teams.KansasCity, Positions.WR));
+    myTeam.addPlayer(new Player('T.Y. Hilton', Teams.Indianapolis, Positions.WR));
+    myTeam.addPlayer(new Player('Travis Kelce', Teams.KansasCity, Positions.TE));
+    myTeam.addPlayer(new Player('Cooper Kupp', Teams.LAR, Positions.WR));
+    myTeam.addPlayer(new Player('Marqise Lee', Teams.Jacksonville, Positions.WR));
+    myTeam.addPlayer(new Player('Marlon Mack', Teams.Indianapolis, Positions.RB));
+    myTeam.addPlayer(new Player('Philip Rivers', Teams.LAC, Positions.QB));
+    myTeam.addPlayer(new Player('Sammy Watkins', Teams.KansasCity, Positions.WR));
+    myTeam.addPlayer(new Player('Stephen Hauschka', Teams.Buffalo, Positions.K));
+    myTeam.addPlayer(new Player('Los Angeles', Teams.LAR, Positions.DEF));
+    return myTeam;
+  }
+
   robbDrafted(): Team {
     var myTeam = new Team();
     myTeam.addPlayer(new Player('LeSean McCoy', Teams.Buffalo, Positions.RB));
@@ -382,6 +579,26 @@ export class IdiotsComponent implements OnInit {
     myTeam.addPlayer(new Player('New England', Teams.NewEngland, Positions.DEF));
     myTeam.addPlayer(new Player('DeAndre Washington', Teams.Oakland, Positions.RB));
     myTeam.addPlayer(new Player('Adam Vinatieri', Teams.Indianapolis, Positions.K));
+    return myTeam;
+  }
+
+  robbFinal(): Team {
+    var myTeam = new Team();
+    myTeam.addPlayer(new Player('Tom Brady', Teams.NewEngland, Positions.QB));
+    myTeam.addPlayer(new Player('Todd Gurley II', Teams.LAC, Positions.RB));
+    myTeam.addPlayer(new Player('Austin Hooper', Teams.Atlanta, Positions.TE));
+    myTeam.addPlayer(new Player('Allen Hurns', Teams.Jacksonville, Positions.WR));
+    myTeam.addPlayer(new Player('Rishard Matthews', Teams.Tennessee, Positions.WR));
+    myTeam.addPlayer(new Player('Lamar Miller', Teams.Houston, Positions.RB));//*
+    myTeam.addPlayer(new Player('Latavius Murray', Teams.Minnesota, Positions.RB));
+    myTeam.addPlayer(new Player('Ben Roethlisberger', Teams.Pittsburgh, Positions.QB));//*
+    myTeam.addPlayer(new Player('Kyle Rudolph', Teams.Minnesota, Positions.TE));
+    myTeam.addPlayer(new Player('JuJu Smith-Schuster', Teams.Pittsburgh, Positions.WR));
+    myTeam.addPlayer(new Player('Kenny Stills', Teams.NewOrleans, Positions.WR));
+    myTeam.addPlayer(new Player('Golden Tate', Teams.Detroit, Positions.WR));
+    myTeam.addPlayer(new Player('Josh Lambo', Teams.Jacksonville, Positions.K));
+    myTeam.addPlayer(new Player('Chicago', Teams.Chicago, Positions.DEF));
+    myTeam.addPlayer(new Player('New Orleans', Teams.NewOrleans, Positions.DEF));
     return myTeam;
   }
 
@@ -405,6 +622,26 @@ export class IdiotsComponent implements OnInit {
     return myTeam;
   }
 
+  steveFinal(): Team {
+    var myTeam = new Team();
+    myTeam.addPlayer(new Player('C.J. Anderson', Teams.Carolina, Positions.RB));
+    myTeam.addPlayer(new Player('Dez Bryant', Teams.Dallas, Positions.WR));
+    myTeam.addPlayer(new Player('Corey Coleman', Teams.Buffalo, Positions.WR));
+    myTeam.addPlayer(new Player('James Conner', Teams.Pittsburgh, Positions.RB));
+    myTeam.addPlayer(new Player('Amari Cooper', Teams.Oakland, Positions.WR));
+    myTeam.addPlayer(new Player('Jack Doyle', Teams.Indianapolis, Positions.TE));
+    myTeam.addPlayer(new Player('Evan Engram', Teams.NYG, Positions.TE));
+    myTeam.addPlayer(new Player('Devonta Freeman', Teams.Atlanta, Positions.RB));
+    myTeam.addPlayer(new Player('Case Keenum', Teams.Minnesota, Positions.QB));
+    myTeam.addPlayer(new Player('Tyler Kroft', Teams.Cincinnati, Positions.TE));
+    myTeam.addPlayer(new Player('LeSean McCoy', Teams.Buffalo, Positions.RB));
+    myTeam.addPlayer(new Player('Rod Smith', Teams.Dallas, Positions.RB));
+    myTeam.addPlayer(new Player('Dede Westrbook', Teams.Jacksonville, Positions.WR));
+    myTeam.addPlayer(new Player('Chris Boswell', Teams.Pittsburgh, Positions.K));
+    myTeam.addPlayer(new Player('New York Jets', Teams.NYJ, Positions.DEF));
+    return myTeam;
+  }
+
   meDrafted(): Team {
     var myTeam = new Team();
     myTeam.addPlayer(new Player('Devonta Freeman', Teams.Atlanta, Positions.RB));
@@ -421,6 +658,26 @@ export class IdiotsComponent implements OnInit {
     myTeam.addPlayer(new Player('Kenny Golladay', Teams.Detroit, Positions.WR));
     myTeam.addPlayer(new Player('Carson Palmer', Teams.Arizona, Positions.QB));
     myTeam.addPlayer(new Player('Mason Crosby', Teams.GreenBay, Positions.K));
+    myTeam.addPlayer(new Player('Baltimore', Teams.Baltimore, Positions.DEF));
+    return myTeam;
+  }
+
+  meFinal(): Team {
+    var myTeam = new Team();
+    myTeam.addPlayer(new Player('Tevin Coleman', Teams.Atlanta, Positions.RB));
+    myTeam.addPlayer(new Player('Brandin Cooks', Teams.LAR, Positions.WR));
+    myTeam.addPlayer(new Player('Michael Grabtree', Teams.Baltimore, Positions.WR));
+    myTeam.addPlayer(new Player('Vernon Davis', Teams.SanFrancisco, Positions.TE));
+    myTeam.addPlayer(new Player('Ed Dickson', Teams.Carolina, Positions.TE));
+    myTeam.addPlayer(new Player('Will Fuller V', Teams.Houston, Positions.WR));
+    myTeam.addPlayer(new Player('Ted Ginn Jr.', Teams.NewOrleans, Positions.WR));
+    myTeam.addPlayer(new Player('Frank Gore', Teams.Miami, Positions.RB));
+    myTeam.addPlayer(new Player('Doug Martin', Teams.Oakland, Positions.RB));
+    myTeam.addPlayer(new Player('Alex Smith', Teams.KansasCity, Positions.QB));
+    myTeam.addPlayer(new Player('James White', Teams.NewEngland, Positions.RB));
+    myTeam.addPlayer(new Player('Russell Wilson', Teams.Seattle, Positions.QB));
+    myTeam.addPlayer(new Player('Mason Crosby', Teams.GreenBay, Positions.K));
+    myTeam.addPlayer(new Player('Robbie Gould', Teams.SanFrancisco, Positions.K));
     myTeam.addPlayer(new Player('Baltimore', Teams.Baltimore, Positions.DEF));
     return myTeam;
   }
